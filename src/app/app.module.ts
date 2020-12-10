d9aada8c0ae41ba636db17bc54a1f1bac8d3fba7
iheb0510 <60815101+iheb0510@users.noreply.github.com>
2020-12-10 02:26:32 +0100

    Mini Projet Angular iheb laribi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ListeRoomsComponent} from './liste-rooms/liste-rooms.component';
+import {RoomComponent} from './room/room.component';
+import {AjouterRoomComponent} from './ajouter-room/ajouter-room.component';
+import {ModifierRoomComponent} from './modifier-room/modifier-room.component';
+import {AppRoutingModule} from './app.routing.module';
+import {AjouterTouristComponent} from './ajouter-tourist/ajouter-tourist.component';
+import {ModifierTouristComponent} from './modifier-tourist/modifier-tourist.component';
+import {DeleteRoomComponent} from './delete-room/delete-room.component';
+import {ListTouristsComponent} from './list-tourists/list-tourists.component';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeRoomsComponent,
+    RoomComponent,
+    AjouterRoomComponent,
+    ModifierRoomComponent,
+    AjouterTouristComponent,
+    ModifierTouristComponent,
+    ListTouristsComponent,
+    DeleteRoomComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
